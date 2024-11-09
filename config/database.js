04a17b9a53ab0098d8dfe021ba9d4d722082490a
shihabmoni <shihabmoni15@gmail.com>
2024-11-09 03:20:24 -0800

    first commicsadsdt

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 // mongodb+srv://shihabmoni15:XWn73HO5A9xScTtN@cluster0.tbovh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 function database(){
 
-    mongoose.connect("mongodb+srv://shihabmoni15:bg2dK35zOrGrF8TG@cluster0.x2ip4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    mongoose.connect("mongodb+srv://shihabmoni15:k6Qa05zgEdHUtNIi@cluster0.xifoh.mongodb.net/")
     .then((res)=>{
         console.log("Database connected....")
     }).catch((err)=>{
